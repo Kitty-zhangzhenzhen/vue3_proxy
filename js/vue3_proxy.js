668ce652950b3a3042d8aab52240daa7d913a6b8
Kitty-zhangzhenzhen <929041292@qq.com>
2021-06-07 16:05:12 +0800

    输入双向绑定

--- js/vue3_proxy.js
+++ js/vue3_proxy.js
@@ -1,62 +1,100 @@
-    // 对代理过的对象做缓存操作
-    const toProxy = new WeakMap(); // 存放代理后的对象
-    const toRaw = new WeakMap();  //  存放代理前的对象
-    
-    function triger(){
-        console.log('触发视图更新');
-   }
+const isObject = v=> v!==null && typeof v ==='object'
 
-   // 判断是否是对象
-   function isObject(target){
-        return typeof target === 'object' && target !== null;
-   }
+/* 建立响应式数据 */
+function reactive(obj){
+  // Proxy:http://es6.ruanyifeng.com/#docs/proxy
+  // Proxy相当于在对象外层加拦截
+  // Proxy递归是惰性的,需要添加递归的逻辑
+  
+  // Reflect:http://es6.ruanyifeng.com/#docs/reflect
+  // Reflect:用于执行对象默认操作，更规范、更友好,可以理解成操作对象的合集
+  // Proxy和Object的方法Reflect都有对应
+  if(!isObject(obj)) return obj
+  const observed = new Proxy(obj,{
+    get(target, key, receiver){
+      const ret = Reflect.get(target, key, receiver)
+      console.log('getter '+ret)
+      // 跟踪 收集依赖
+      track(target, key)
+      return reactive(ret)
+    },
+    set(target, key, val, receiver){
+      const ret = Reflect.set(target, key, val, receiver)
+      console.log('setter '+key+':'+val + '=>' + ret)
+      // 触发更新
+      trigger(target, key)
+      return ret
+    },
+    deleteProperty(target, key){
+      const ret = Reflect.deleteProperty(target, key)
+      console.log('delete '+key+':'+ret)
+      // 触发更新
+      trigger(target, key)
+      return ret
+    },
+  })
+  return observed
+}
 
-   function reactive(target){
-       if(isObject(target)){
-           return target
-       } 
-       // 如果代理表中已经存在了，就把结果返回       let a = {name:'yp'} ; p = reactive(a) ; p = reactive(a); p = reactive(a)
-       let proxy = toProxy.get(target); 
-       if(proxy){  // 如果有值，就说明代理过了，直接返回
-           return toProxy.get(target);
-       }
-       // 如果这个对象已经被代理过了，就把对象返回  let a = {name:'yp'} ; let p = reactive(a) ; p = reactive(p); p = reactive(p)
-       if(toRaw.has(target)){
-           return target
-       }
-       const handlers = {  // 触发的方法
-            set(target,key,value,receiver){
-                //  console.log(key)  修改数组值的时候,key有两个修改的值和length
-                if(target.hasOwnProperty(key)){ // 即如果触发的是私有属性，可以直接触发视图更新,length会屏蔽掉
-                    triger();
-                }
-                return Reflect.set(target, key, value,receiver)
-            },
-            get(target,key,receiver){
-                const res = Reflect.get(target, key, receiver);
-                if(isObject(target[key])){
-                    return reactive(res)
-                }
-                return res;
+/* 声明响应函数cb */
+const effectStack = []
+function effect(cb){
 
-            },
-            deleteProperty(target,key){
-                Reflect.deleteProperty(target, key)
-            }
-       }
-       let observed = new Proxy(target,handlers)  // 对代理target，做handlers方法处理
-       toProxy.set(target,observed) // 原对象 代理后的结果
-       toRaw.set(observed,target)
-       return observed
-   }
-   
-   let obj = {
-        name:'yp',
-        a:[1,2,3,4]
-   }
+  // 对函数进行高阶封装
+  const rxEffect = function(){
+    // 1.捕获异常
+    // 2.fn出栈入栈
+    // 3.执行fn
+    try{
+      effectStack.push(rxEffect)
+      return cb()
+    }finally{
+      effectStack.pop()
+    }
+  }
 
-   let p = reactive(obj)
+  // 最初要执行一次,进行最初的依赖收集
+  rxEffect()
 
-   p.name = 'yp3'
+  return rxEffect
+}
 
-   console.log(obj)
+/* 依赖收集：建立 数据&cb 映射关系 */
+const targetMap = new WeakMap()
+function track(target,key){
+  // 存入映射关系
+  const effectFn = effectStack[effectStack.length - 1]  // 拿出栈顶函数
+  if(effectFn){
+    let depsMap = targetMap.get(target)
+    if(!depsMap){
+      depsMap = new Map()
+      targetMap.set(target, depsMap)
+    }
+    let deps = depsMap.get(key)
+    if(!deps){
+      deps = new Set()
+      depsMap.set(key, deps)
+    }
+    deps.add(effectFn)
+  }
+}
+
+/* 触发更新：根据映射关系，执行cb */
+function trigger(target, key){
+  const depsMap = targetMap.get(target)
+  if(depsMap){
+    const deps = depsMap.get(key)
+    if(deps){
+      deps.forEach(effect=>effect())
+    }
+  }
+}
+
+
+
+// const state = reactive({foo:'foo', bar:{a:1}, arr:[1,2,3]})
+// // effect里传入一个会触发getter的回调函数
+// effect(()=>{
+//   console.log(state.foo)
+// })
+// state.foo = 'bar'
